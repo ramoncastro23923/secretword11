93c97a841400e0eef3559e06d2c19fc47c32a5fa
ramoncastro674 <tadeuramoncastro@gmail.com>
2024-02-15 10:58:48 -0300

    first commit

---
+++ src/components/GameOver.js
@@ -0,0 +1,15 @@
+import "./GameOver.css";
+
+const GameOver = ({ retry, score }) => {
+    return (
+      <>
+          <div>
+            <h1>Fim de Jogo!</h1>
+            <p>A sua pontuação foi: <span>{score}</span></p>
+            <button onClick={retry}>Resetar jogo</button>
+          </div>
+      </>
+    )
+  }
+  
+  export default GameOver;
---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordsList = {
+    carro: ["Motor", "Roda", "Capo", "Pneu", "Volante"],
+    fruta: ["Banana", "Maça", "Pera", "Mamao", "Laranja"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "Javascript", "React"],
+    alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo"],
+    casa: ["quintal", "banheiro", "janela", "porta", "cozinha" ],
+    escola: ["lapis", "borracha", "apontador","caderno", "caneta"],
+};
